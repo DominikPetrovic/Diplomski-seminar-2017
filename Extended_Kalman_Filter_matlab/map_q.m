ce715c892d09a9682fd73e66f158625ecafa8063
Kunsstek <zvonimir.kunstek@fer.hr>
2017-05-12 10:45:31 +0200

    Changes in EKF_matlab folder

--- Extended_Kalman_Filter_matlab/map_q.m
+++ Extended_Kalman_Filter_matlab/map_q.m
@@ -3,7 +3,11 @@ function mapped_q=map_q(fi)
 %   kvaternion mapped_q je 4x1
 %   pomak kuta Dfi je 3x1
 
+if(fi(1)==0 && fi(2)==0 && fi(3)==0)
+   pom1=[0;0;0];
+else
 pom1=sin(0.5*norm(fi))*fi/norm(fi);
+end
 pom2=cos(0.5*norm(fi));
 mapped_q=[pom1;pom2];
 
